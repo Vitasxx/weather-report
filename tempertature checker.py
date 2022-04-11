dc10380054eb53f1ead7bb25f993f5592520db2d
Vitasxx <98189034+Vitasxx@users.noreply.github.com>
2022-04-11 23:34:48 +0100

    Add files via upload

---
+++ tempertature checker.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+def square(x):
+    return x*x 
+
+def get_temperature(city):
+    url = "https://wttr.in/" + city + "?format=%t"
+    page = urlopen(url)
+    raw=page.read()
+    temp = raw.decode("utf-8")
+    return temp
+
+city = input("city:")
+temp = get_temperature(city)
+print(temp)    
